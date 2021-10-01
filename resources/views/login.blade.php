e5bb5c7f47eb4348b38a78e741c98668e7b8e926
AnungFirdauzy <anungfirdauzy538@gmail.com>
2021-10-01 18:47:17 +0700

    Ubah Format Penyerjaan Layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login MyCica</title>
+@extends('layouts.main')
 
-        {{-- Bootstrap --}}
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-        />
-
-        {{-- CSS --}}
-        <link rel="stylesheet" href="css/lp-before-login.css" />
-
-        {{-- Font --}}
-            {{-- Montserrat --}}
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-</head>
-<body>
+@section('container')
     <div class="container-fluid vh-100">
         <div class="container position-absolute top-50 start-50 translate-middle">
             <div class="container" id="Upper">
@@ -74,10 +51,10 @@
         </div>
         </div>
     </div>
+@endsection
 
 
-
-
+@section('script')
 <script
 src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
@@ -87,7 +64,6 @@ var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-})</script>
-</body>
-</html>
+    myInput.focus()})
+</script>
+@endsection
